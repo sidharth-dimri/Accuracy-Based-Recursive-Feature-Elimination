6aa7db86c50b63a9d5e9278ba53e5efa206fb96c
Sidharth Dimri <sidharthdimri66@gmail.com>
2025-04-11 19:44:42 -0400

    Update Accuracy-based_Recursive_Feature_Elimination.py
    Replaced previous code with a version that allows for hyperparameter input, along with instructions on how to create a method for this algorithm using other ML models.

--- Accuracy-based_Recursive_Feature_Elimination.py
+++ Accuracy-based_Recursive_Feature_Elimination.py
@@ -8,8 +8,18 @@ import numpy as np
 This example uses KNN, but this method can be used with any ML model that has some sort of accuracy to it.
 For example, regression models can use Mean Squared Error (find the lowest MSE)
 """
-#function should be first called by x = ARFE(x,y)
-def ARFE(x,y,avgScoreOriginal=0):
+
+"""
+To implement this model with any other ML model, follow these steps:
+1. Import the library of the desired model
+2. Replace the hyperparameters (any input after the third in ARFE) with ALL of the hyperparameters of your desired model, setting the default value as required
+3. Replace calls to the model with your desired model, including the model's hyperparameters
+4. Make sure the hyperparameters inputted into the recursive call are replaced with the hyperparameters of your model
+"""
+
+#after the third input, the rest are knn hyperparameters
+#default values are set so that if no hyperparameters are given, a default model runs
+def ARFE(x,y,avgScoreOriginal=0, n_neighbors=5, weights='uniform', algorithm='auto', leaf_size=30, p=2, metric='minkowski', metric_params=None, n_jobs=None):
     #if starting model score >= to normal, function ends
     #test original model
     """
@@ -21,9 +31,9 @@ def ARFE(x,y,avgScoreOriginal=0):
     However, you can call the function outside of recursion with just ARFE(x,y), since calculating avgScoreOriginal then saves no run time
     """
     if(avgScoreOriginal==0):
-        knn = KNeighborsClassifier()
-        knn.fit(x,y)
-        cvScore = cross_val_score(knn, x, y, cv=4)
+        model = KNeighborsClassifier(n_neighbors=n_neighbors, weights=weights, algorithm=algorithm, leaf_size=leaf_size, p=p, metric=metric, metric_params=metric_params, n_jobs=n_jobs)
+        model.fit(x,y)
+        cvScore = cross_val_score(model, x, y, cv=4)
         avgScoreOriginal = np.average(cvScore)
         avgScoreOriginal = avgScoreOriginal*100
     
@@ -32,9 +42,9 @@ def ARFE(x,y,avgScoreOriginal=0):
     avgScoreRemoved = [0, -1]
     for k in range(x.shape[1]):
         x2 = x.drop(x.columns[k],axis=1)
-        knn = KNeighborsClassifier()
-        knn.fit(x2,y)
-        cvScore = cross_val_score(knn, x2, y, cv=4)
+        model = KNeighborsClassifier(n_neighbors=n_neighbors, weights=weights, algorithm=algorithm, leaf_size=leaf_size, p=p, metric=metric, metric_params=metric_params, n_jobs=n_jobs)
+        model.fit(x2,y)
+        cvScore = cross_val_score(model, x2, y, cv=4)
         avgScore = np.average(cvScore)
         avgScore = avgScore*100
         if(avgScore>avgScoreRemoved[0]):
@@ -44,7 +54,6 @@ def ARFE(x,y,avgScoreOriginal=0):
     if(avgScoreRemoved[0]>avgScoreOriginal):
         avgScoreOriginal = avgScoreRemoved[0]
         x = x.drop(x.columns[avgScoreRemoved[1]], axis=1)
-        return ARFE(x,y,avgScoreOriginal)
+        return ARFE(x,y,avgScoreOriginal, n_neighbors, weights, algorithm, leaf_size, p, metric, metric_params, n_jobs)
     else:
         return x
-
